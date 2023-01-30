bf37cbd8e41b7a0c45e9d657c1b2e40f783b9fcc
apnijava <vijaydehraj15@gmail.com>
2023-01-30 18:02:52 +0530

    Code to practice

--- src/com/practice/DeleteAndPractice.java
+++ src/com/practice/DeleteAndPractice.java
@@ -3,8 +3,10 @@ package com.practice;
 public class DeleteAndPractice {
 
 	public static void main(String[] args) {
-	
 
+		int num = 25625;
+		int lastdig = num % 100;
+		System.out.println(lastdig);
 	}
 
 }
