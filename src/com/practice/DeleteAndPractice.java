6de344c66a7e16b78cca9cfc49b83c76e805116d
apnijava <vijaydehraj15@gmail.com>
2023-02-01 12:25:53 +0530

    changes due to practice

--- src/com/practice/DeleteAndPractice.java
+++ src/com/practice/DeleteAndPractice.java
@@ -4,8 +4,8 @@ public class DeleteAndPractice {
 
 	public static void main(String[] args) {
 
-		int num = 25625;
-		int lastdig = num % 100;
+		int num = 121;
+		int lastdig = num %100;
 		System.out.println(lastdig);
 	}
 
