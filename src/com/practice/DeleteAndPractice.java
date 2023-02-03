7eee2e7c41509eec6ea0250c0fcfbb6b60e91387
apnijava <vijaydehraj15@gmail.com>
2023-02-03 11:09:10 +0530

    Changes due to practice

--- src/com/practice/DeleteAndPractice.java
+++ src/com/practice/DeleteAndPractice.java
@@ -3,10 +3,17 @@ package com.practice;
 public class DeleteAndPractice {
 
 	public static void main(String[] args) {
-
-		int num = 121;
-		int lastdig = num %100;
-		System.out.println(lastdig);
+		// int profit = 0;
+		// System.out.println(min);
+		int[] prices = { 54, 2, 6, 787, 112, -7 };
+		int max = prices[0];
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] > max) {
+				max = prices[i];
+			}
+		}
+		System.out.println(max);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
 	}
-
 }
