172455e5868f5a2a1d326cb76bf1b8162145397a
apnijava <vijaydehraj15@gmail.com>
2023-03-29 14:39:21 +0530

    added another print statement in stream.map

--- src/com/stream/StreamWithMap.java
+++ src/com/stream/StreamWithMap.java
@@ -23,9 +23,13 @@ public class StreamWithMap {
 		List<Integer> UpdatedMarks = marks.stream().map(i -> i + 5).collect(Collectors.toList());
 		System.out.println("The Updated Marks of the students are: " + UpdatedMarks);
 
+<<<<<<< HEAD
 		List<Integer> modifiedmarks = marks.stream().map(i -> i + 5).collect(Collectors.toList()).stream().map(i->i+10).collect(Collectors.toList());
 		System.out.println("The Updated Marks of the students are: " + modifiedmarks);
 
+=======
+		
+>>>>>>> 5ed2821c4037627f2b9e47a3eaf7301d1ac82e8e
 	}
 
 }
